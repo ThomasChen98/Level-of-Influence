55d0348232dab6aab7518cd0731add7d0c2b483d
Yuxin Chen <yuxinc@berkeley.edu>
2023-08-06 16:02:51 -0700

    separate config

---
+++ MARL/view_model.py
@@ -0,0 +1,200 @@
+# Copyright 2020 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Runs the bots trained in self_play_train.py and renders in pygame.
+
+You must provide experiment_state, expected to be
+~/ray_results/PPO/experiment_state_YOUR_RUN_ID.json
+"""
+
+import os
+import dm_env
+from dmlab2d.ui_renderer import pygame
+import numpy as np
+from ray.rllib.algorithms.registry import get_trainer_class
+from ray.tune.analysis.experiment_analysis import ExperimentAnalysis
+from ray.tune.registry import register_env
+from ray.rllib.algorithms import ppo
+from ray.rllib.policy.policy import PolicySpec
+from ray.rllib.algorithms.ppo import PPO
+
+from examples.rllib import utils
+from meltingpot.python import substrate
+
+def get_config(
+    substrate_name: str = "stag_hunt_in_the_matrix__repeated_small",
+    num_rollout_workers: int = 2,
+    rollout_fragment_length: int = 100,
+    train_batch_size: int = 1600,
+    fcnet_hiddens=(64, 64),
+    post_fcnet_hiddens=(256,),
+    lstm_cell_size: int = 256,
+    sgd_minibatch_size: int = 128,
+    
+):
+  """Get the configuration for running an agent on a substrate using RLLib.
+
+  We need the following 2 pieces to run the training:
+
+  Args:
+    substrate_name: The name of the MeltingPot substrate, coming from
+      `substrate.AVAILABLE_SUBSTRATES`.
+    num_rollout_workers: The number of workers for playing games.
+    rollout_fragment_length: Unroll time for learning.
+    train_batch_size: Batch size (batch * rollout_fragment_length)
+    fcnet_hiddens: Fully connected layers.
+    post_fcnet_hiddens: Layer sizes after the fully connected torso.
+    lstm_cell_size: Size of the LSTM.
+    sgd_minibatch_size: Size of the mini-batch for learning.
+
+  Returns:
+    The configuration for running the experiment.
+  """
+  # Gets the default training configuration
+  config = ppo.PPOConfig()
+  # Number of arenas.
+  # This is called num_rollout_workers in 2.2.0.
+  config.num_workers = num_rollout_workers
+  # This is to match our unroll lengths.
+  config.rollout_fragment_length = rollout_fragment_length
+  # Total (time x batch) timesteps on the learning update.
+  config.train_batch_size = train_batch_size
+  # Mini-batch size.
+  config.sgd_minibatch_size = sgd_minibatch_size
+  # Use the raw observations/actions as defined by the environment.
+  config.preprocessor_pref = None
+  # Use TensorFlow as the tensor framework.
+  config = config.framework("tf")
+  # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+  config.num_gpus = int(os.environ.get("RLLIB_NUM_GPUS", "0"))
+  config.log_level = "DEBUG"
+
+  # 2. Set environment config. This will be passed to
+  # the env_creator function via the register env lambda below.
+  player_roles = substrate.get_config(substrate_name).default_player_roles
+  config.env_config = {"substrate": substrate_name, "roles": player_roles}
+
+  config.env = "meltingpot"
+
+  # 4. Extract space dimensions
+  test_env = utils.env_creator(config.env_config)
+
+  # Setup PPO with policies, one per entry in default player roles.
+  policies = {}
+  player_to_agent = {}
+  for i in range(len(player_roles)):
+    rgb_shape = test_env.observation_space[f"player_{i}"]["RGB"].shape
+    sprite_x = rgb_shape[0] // 8
+    sprite_y = rgb_shape[1] // 8
+
+    policies[f"agent_{i}"] = PolicySpec(
+      policy_class=None,  # use default policy
+      observation_space=test_env.observation_space[f"player_{i}"],
+      action_space=test_env.action_space[f"player_{i}"],
+      config={
+        "model": {
+          "conv_filters": [[16, [8, 8], 8],
+                           [128, [sprite_x, sprite_y], 1]],
+        },
+      })
+    player_to_agent[f"player_{i}"] = f"agent_{i}"
+
+  def policy_mapping_fn(agent_id, **kwargs):
+    del kwargs
+    return player_to_agent[agent_id]
+
+  # 5. Configuration for multi-agent setup with one policy per role:
+  config.multi_agent(policies=policies, policy_mapping_fn=policy_mapping_fn, policies_to_train=["agent_0"])
+
+  # 6. Set the agent architecture.
+  # Definition of the model architecture.
+  # The strides of the first convolutional layer were chosen to perfectly line
+  # up with the sprites, which are 8x8.
+  # The final layer must be chosen specifically so that its output is
+  # [B, 1, 1, X]. See the explanation in
+  # https://docs.ray.io/en/latest/rllib-models.html#built-in-models. It is
+  # because rllib is unable to flatten to a vector otherwise.
+  # The acb models used as baselines in the meltingpot paper were not run using
+  # rllib, so they used a different configuration for the second convolutional
+  # layer. It was 32 channels, [4, 4] kernel shape, and stride = 1.
+  config.model["fcnet_hiddens"] = fcnet_hiddens
+  config.model["fcnet_activation"] = "relu"
+  config.model["conv_activation"] = "relu"
+  config.model["post_fcnet_hiddens"] = post_fcnet_hiddens
+  config.model["post_fcnet_activation"] = "relu"
+  config.model["use_lstm"] = True
+  config.model["lstm_use_prev_action"] = True
+  config.model["lstm_use_prev_reward"] = False
+  config.model["lstm_cell_size"] = lstm_cell_size
+
+  return config
+
+def main():
+  config = get_config()
+  checkpoint_path = '/home/yuxin/ray_results/PPO/PPO_meltingpot_stag_hunt_O_5M/checkpoint_003125'
+
+  trainer = get_trainer_class('PPO')(config=config)
+  trainer.restore(checkpoint_path)
+
+  # Create a new environment to visualise
+  env = utils.env_creator(config["env_config"]).get_dmlab2d_env()
+
+  bots = [
+      utils.RayModelPolicy(trainer, f"agent_{i}")
+      for i in range(len(config["env_config"]["roles"]))
+  ]
+
+  timestep = env.reset()
+  states = [bot.initial_state() for bot in bots]
+  actions = [0] * len(bots)
+
+  # Configure the pygame display
+  scale = 8
+  fps = 5
+
+  pygame.init()
+  clock = pygame.time.Clock()
+  pygame.display.set_caption("DM Lab2d")
+  obs_spec = env.observation_spec()
+  shape = obs_spec[0]["WORLD.RGB"].shape
+  game_display = pygame.display.set_mode(
+      (int(shape[1] * scale), int(shape[0] * scale)))
+
+  for _ in range(10):
+    obs = timestep.observation[0]["WORLD.RGB"]
+    obs = np.transpose(obs, (1, 0, 2))
+    surface = pygame.surfarray.make_surface(obs)
+    rect = surface.get_rect()
+    surf = pygame.transform.scale(surface,
+                                  (int(rect[2] * scale), int(rect[3] * scale)))
+
+    game_display.blit(surf, dest=(0, 0))
+    pygame.display.update()
+    save_name = "/home/yuxin/Downloads/"+str(_)+".jpeg"
+    pygame.image.save(game_display, save_name)
+    clock.tick(fps)
+
+    for i, bot in enumerate(bots):
+      timestep_bot = dm_env.TimeStep(
+          step_type=timestep.step_type,
+          reward=timestep.reward[i],
+          discount=timestep.discount,
+          observation=timestep.observation[i])
+
+      actions[i], states[i] = bot.step(timestep_bot, states[i])
+
+    timestep = env.step(actions)
+
+
+if __name__ == "__main__":
+  main()
